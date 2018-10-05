24155b93488ff0944c1b53b4816b181c6ecc9e6f
Ki Song <kiwisquash@gmail.com>
2018-10-04 23:14:45 -0700

    testing scrip not working well.

--- player.py
+++ player.py
@@ -18,3 +18,16 @@ class Player:
         self.y=+1
     def moveD(self):
         self.y=-1
+
+def testPlayer(x,y):
+    bob = Player(x,y)
+    print("Start from the point ("+bob.getX()+","+bob.getY()+") let's move.")
+    bob.moveR()
+    bob.moveU()
+    bob.moveR()
+    bob.moveL()
+    bob.moveD()
+    bob.moveD()
+    print("End up at ("+bob.getX()+","+bob.getY()+").")
+
+testPlayer(0,0)
