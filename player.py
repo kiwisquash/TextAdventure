bf184dce1460a48dc43fd7c8cf762d0a669d10d1
Ki Song <kiwisquash@gmail.com>
2018-10-05 23:15:16 -0700

    Added position method and a method for printing the position. Also updated the test function.

--- player.py
+++ player.py
@@ -11,29 +11,45 @@ class Player:
     def setY(self,y):
         self.y = y
     def moveL(self):
-        self.x=-1
+        self.x-=1
     def moveR(self):
-        self.x=+1
+        self.x+=1
     def moveU(self):
-        self.y=+1
+        self.y+=1
     def moveD(self):
-        self.y=-1
+        self.y-=1
+    def position(self):
+        output = "("
+        output += str(self.getX())
+        output +=", "
+        output += str(self.getY())
+        output += ")"
+        return output
+    def printPos(self):
+        output = "Your current position is "
+        output += self.position()
+        print(output)
 
 def testPlayer(x,y):
     bob = Player(x,y)
     print("Start from the point ("+str(bob.getX())+","+str(bob.getY())+") let's move.")
     bob.moveR()
-    print("Move right.")
+    bob.printPos()
     bob.moveU()
     print("Move up.")
+    bob.printPos()
     bob.moveR()
     print("Move right.")
+    bob.printPos()
     bob.moveL()
     print("Move left.")
+    bob.printPos()
     bob.moveD()
     print("Move down.")
+    bob.printPos()
     bob.moveD()
     print("Move down.")
+    bob.printPos()
     print("End up at ("+str(bob.getX())+","+str(bob.getY())+").")
 
-testPlayer(0,0)
+testPlayer(0,3)
