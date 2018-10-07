9d33348752d3fbb8fcdded00fdbbacff9bc847af
Ki Song <kiwisquash@gmail.com>
2018-10-07 00:26:29 -0700

    Added rooms.py

---
+++ rooms.py
@@ -0,0 +1,9 @@
+class Room:
+    def __init__(self,message):
+        self.message = message
+    def roomAnnounce(self):
+        print(self.message)
+    def affectPlayer(self,player):
+        pass
+
+
