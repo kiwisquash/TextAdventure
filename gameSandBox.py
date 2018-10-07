6f212cf45f066a805d76b2b2c3100aa6df9e7601
Ki Song <kiwisquash@gmail.com>
2018-10-07 01:44:20 -0700

    Added while loops for game mechanic>

--- gameSandBox.py
+++ gameSandBox.py
@@ -1,6 +1,29 @@
+from player import Player
 
+print("Welcome to adventures of Bob.")
+print("Would you like to play? (y/n)")
+response = input()
+again = "y"
 
-print("Do you want to quit? ","")
-quit = ""
-while (quit!="q"): 
-    quit = input()
+while(again=="y"):
+    while response !="y" and response !="n":
+        print("I'm sorry, I didn't understand your response.")
+        print("Would you like to play? (y/n)")
+        response = input()
+    if response == "y":
+        bob = Player()
+        while (bob.getStatus() == False):
+            print("Do you want to win?")
+            response = input()
+            if response == "y":
+                bob.win()
+                print("Great! You won!")
+            else:
+                print("Suit yourself.")
+        print("Do you want to play again?")
+        again = input()
+    else:
+        print("Okay, have a nice day!")
+        again = "n"
+print("Hope you had fun!")
+
