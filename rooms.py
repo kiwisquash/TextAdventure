85d2815569d51a16d242661150d7336d9689c6ee
Ki Song <kiwisquash@gmail.com>
2018-10-07 12:06:04 -0700

    Created an 'Exit' room and created a simple random room generator.

--- rooms.py
+++ rooms.py
@@ -1,9 +1,28 @@
+import random
+
 class Room:
     def __init__(self,message):
         self.message = message
-    def roomAnnounce(self):
+    def getMessage(self):
+        return self.message
+    def setMessage(self,message):
+        self.message = message
+    def printMessage(self):
         print(self.message)
     def affectPlayer(self,player):
         pass
 
+class Exit(Room):
+    def affectPlayer(self,player):
+        player.win()
 
+map = []
+exitX = random.randint(0,2)
+exitY = random.randint(0,2)
+for i in range(3):
+    map.append([])
+    for j in range(3):
+        if j == exitY and i == exitX:
+            map[i].append(Exit("This is the exit!"))
+        else:
+            map[i].append(Room("This is a room. Nothing special here))
