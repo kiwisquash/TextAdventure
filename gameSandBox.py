6d6d01e3ad02d40f5c9a49d98ea356a3fde72eba
Ki Song <kiwisquash@gmail.com>
2018-10-07 01:53:37 -0700

    Added some comments. 'have a nice day' message will appear whenever the game ends.

--- gameSandBox.py
+++ gameSandBox.py
@@ -5,14 +5,14 @@ print("Would you like to play? (y/n)")
 response = input()
 again = "y"
 
-while(again=="y"):
-    while response !="y" and response !="n":
+while(again=="y"): 
+    while response !="y" and response !="n": # Try to get a valid answer
         print("I'm sorry, I didn't understand your response.")
         print("Would you like to play? (y/n)")
         response = input()
-    if response == "y":
+    if response == "y": # Game start
         bob = Player()
-        while (bob.getStatus() == False):
+        while (bob.getStatus() == False): # Game start
             print("Do you want to win?")
             response = input()
             if response == "y":
@@ -22,8 +22,9 @@ while(again=="y"):
                 print("Suit yourself.")
         print("Do you want to play again?")
         again = input()
-    else:
-        print("Okay, have a nice day!")
+    else: 
         again = "n"
-print("Hope you had fun!")
+    print("Okay, have a nice day!")
+if response == "y": # Message for someone who played the game at least once.
+    print("Hope you had fun!")
 
